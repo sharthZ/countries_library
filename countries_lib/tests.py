55298e854ee89d4ec1b4ded0531a72058d58049c
Daniel Potapov <sharth23@gmail.com>
2017-11-09 19:00:39 +0300

    Refactor unittests

--- countries_lib/tests.py
+++ countries_lib/tests.py
@@ -1,91 +1,121 @@
 # -*- coding: utf-8 -*-
 import unittest
-from country import normalize_country_name, match_country_name, del_country_name
+from country import CountryNormalizer
 
 
-class NormalizeCountryNameTestCase(unittest.TestCase):
+class MatchCountryNameTestCase(unittest.TestCase):
+
+    def run(self, result=None):
+        with CountryNormalizer() as country_normalizer:
+            self.cn = country_normalizer
+            super(MatchCountryNameTestCase, self).run(result)
 
     def test_simple_name(self):
-        self.assertEqual(normalize_country_name('Russia'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russia'), 'Russia')
 
     def test_punctuation_sensitivity(self):
-        self.assertEqual(normalize_country_name('Russia!!!:)'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russia!!!:)'), 'Russia')
 
     def test_upper_register(self):
-        self.assertEqual(normalize_country_name('RUSSIA'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('RUSSIA'), 'Russia')
 
     def test_low_register(self):
-        self.assertEqual(normalize_country_name('russia'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('russia'), 'Russia')
 
     def test_missed_letter(self):
-        self.assertEqual(normalize_country_name('Russi'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russi'), 'Russia')
 
     def test_excess_letter(self):
-        self.assertEqual(normalize_country_name('Russiaa'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russiaa'), 'Russia')
 
     def test_another_letter(self):
-        self.assertEqual(normalize_country_name('Rassia'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Rassia'), 'Russia')
 
     def test_simple_two_words_name(self):
-        self.assertEqual(normalize_country_name('Russian Federation'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russian Federation'),
+                         'Russia')
 
     def test_excess_word_name(self):
-        self.assertEqual(normalize_country_name('The Russia'), 'Russia')
+        self.assertEqual(self.cn.match_country_name('The Russia'), 'Russia')
 
     def test_american_paris_like_construction(self):
-        self.assertEqual(normalize_country_name('Paris, USA'), 'United States')
+        self.assertEqual(self.cn.match_country_name('Paris, USA'),
+                         'United States')
 
     def test_standard_accuracy_result(self):
-        self.assertEqual(normalize_country_name('azazaza'), 'None')
+        self.assertEqual(self.cn.match_country_name('azazaza'), None)
 
     def test_correct_accuracy_type(self):
-        self.assertEqual(normalize_country_name('Russia', 0.9), 'Russia')
+        self.assertEqual(self.cn.match_country_name('Russia', 0.9), 'Russia')
 
     def test_incorrect_accuracy_type(self):
-        self.assertEqual(normalize_country_name('Russia', '0.7'), 'Invalid arguments')
-        self.assertEqual(normalize_country_name('Russia', []), 'Invalid arguments')
+        self.assertEqual(self.cn.match_country_name('Russia', '0.7'), None)
+        self.assertEqual(self.cn.match_country_name('Russia', []), None)
 
     def test_incorrect_accuracy_value(self):
-        self.assertEqual(normalize_country_name('Russia', 3.0), 'Invalid arguments')
-        self.assertEqual(normalize_country_name('Russia', -0.5), 'Invalid arguments')
+        self.assertEqual(self.cn.match_country_name('Russia', 3.0), None)
+        self.assertEqual(self.cn.match_country_name('Russia', -0.5), None)
 
 
-class MatchAndDelCountryNameTestCase(unittest.TestCase):
+class AddAndDelCountryNameTestCase(unittest.TestCase):
+
+    def run(self, result=None):
+        with CountryNormalizer() as country_normalizer:
+            self.cn = country_normalizer
+            super(AddAndDelCountryNameTestCase, self).run(result)
 
     def test_non_existing_object_delete(self):
-            del_country_name('SpecialNameForTest')
-            self.assertEqual(normalize_country_name('SpecialNameForTest'), 'None')
+            self.cn.del_country_name('SpecialForTest')
+            self.assertEqual(self.cn.match_country_name('SpecialForTest'),
+                             None)
 
     def test_match(self):
-            match_country_name('SpecialNameForTest', 'SpecialNameForTest')
-            self.assertEqual(normalize_country_name('SpecialNameForTest'), 'SpecialNameForTest')
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest')
+            self.assertEqual(self.cn.match_country_name('SpecialForTest'),
+                             'SpecialForTest')
 
     def test_existing_object_delete(self):
-            del_country_name('SpecialNameForTest')
-            self.assertEqual(normalize_country_name('SpecialNameForTest'), 'None')
+            self.cn.del_country_name('SpecialForTest')
+            self.assertEqual(self.cn.match_country_name('SpecialForTest'),
+                             None)
 
     def test_correct_priority_match(self):
-            match_country_name('SpecialNameForTest', 'SpecialNameForTest', 1)
-            self.assertEqual(normalize_country_name('SpecialNameForTest'), 'SpecialNameForTest')
-            del_country_name('SpecialNameForTest')
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', 1)
+            self.assertEqual(self.cn.match_country_name('SpecialForTest'),
+                             'SpecialForTest')
+            self.cn.del_country_name('SpecialForTest')
 
     def test_incorrect_priority_match(self):
-        self.assertEqual(match_country_name('SpecialNameForTest', 'SpecialNameForTest', 3), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', 'SpecialNameForTest', -5), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', 'SpecialNameForTest', 1.2), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', 'SpecialNameForTest', '1'), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', 'SpecialNameForTest', []), 'Invalid arguments')
+        self.assertEqual(
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', 3),
+            False)
+        self.assertEqual(
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', -5),
+            False)
+        self.assertEqual(
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', 1.2),
+            False)
+        self.assertEqual(
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', '1'),
+            False)
+        self.assertEqual(
+            self.cn.add_country_name('SpecialForTest', 'SpecialForTest', []),
+            False)
 
     def test_incorrect_match(self):
-        self.assertEqual(match_country_name(1, 'SpecialNameForTest'), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', 1), 'Invalid arguments')
-        self.assertEqual(match_country_name([], 'SpecialNameForTest'), 'Invalid arguments')
-        self.assertEqual(match_country_name('SpecialNameForTest', []), 'Invalid arguments')
+        self.assertEqual(self.cn.add_country_name(1, 'SpecialForTest'),
+                         False)
+        self.assertEqual(self.cn.add_country_name('SpecialForTest', 1),
+                         False)
+        self.assertEqual(self.cn.add_country_name([], 'SpecialForTest'),
+                         False)
+        self.assertEqual(self.cn.add_country_name('SpecialForTest', []),
+                         False)
 
     def test_incorrect_delete(self):
-        self.assertEqual(del_country_name(1), 'Invalid arguments')
-        self.assertEqual(del_country_name(1.5), 'Invalid arguments')
-        self.assertEqual(del_country_name([]), 'Invalid arguments')
+        self.assertEqual(self.cn.del_country_name(1), None)
+        self.assertEqual(self.cn.del_country_name(1.5), None)
+        self.assertEqual(self.cn.del_country_name([]), None)
 
 
 if __name__ == '__main__':
