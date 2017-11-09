bfedd33696783576c4b6a625a3ec94d76beaa327
Daniel Potapov <sharth23@gmail.com>
2017-11-09 19:36:55 +0300

    Temporary fixes for setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='countries_lib',
-    version='1.0',
+    version='1.1',
     include_package_data=True,
     packages=find_packages(),
     test_suite='tests',
-    package_data={'countries_lib': ['database/*', 'documentation/*', 'documentation/html_doc/*',
-                                    'documentation/html_doc/_static/*']}
+    package_data={'countries_lib': [
+        'database/*'
+        # 'documentation/*',
+        # 'documentation/html_doc/*',
+        # 'documentation/html_doc/_static/*'
+    ]}
 )
